63738f6793a940f4c5f44ab9bba35cc6e01dde0c
Maria Lamardo <maria@pendo.io>
2019-07-21 16:54:49 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate'
+import VueMask from 'v-mask'
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
+Vue.use(VueMask);
+
 
 new Vue({
   render: h => h(App),
