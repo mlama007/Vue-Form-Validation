4029cd82f2418390b9346a86a71142e844d33fe5
Maria Lamardo <maria@pendo.io>
2019-07-23 21:42:56 -0400

    formatting fot Vulidate

--- src/components/FormVulidate.vue
+++ src/components/FormVulidate.vue
@@ -8,11 +8,19 @@
         <h1>Create Account</h1>
         <h2>Vuelidate</h2>
         <form @submit.prevent="onSubmit" autocomplete="on">
-
           <!-- First Name -->
           <div class="input" :class="{invalid: $v.firstName.$error}">
-            <label for="firstName">First Name (using @blur) <span aria-hidden=true class="required">*</span> </label>
-            <input type="text" id="firstName" @blur="$v.firstName.$touch()" v-model="firstName" required/>
+            <label for="firstName">
+              First Name (using @blur)
+              <span aria-hidden="true" class="required">*</span>
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              @blur="$v.firstName.$touch()"
+              v-model="firstName"
+              required
+            />
             <p
               v-if="!$v.firstName.required && $v.firstName.$error"
               aria-live="assertive"
@@ -24,13 +32,22 @@
             <p
               v-if="!$v.firstName.alpha && $v.firstName.$error"
               aria-live="assertive"
-            >First name must only contain alphabetic characters </p>
+            >First name must only contain alphabetic characters</p>
           </div>
 
           <!-- Last Name -->
           <div class="input" :class="{invalid: $v.lastName.$error}">
-            <label for="lastName">Last Name (using @input) <span aria-hidden=true class="required">*</span></label>
-            <input type="text" id="lastName" @input="$v.lastName.$touch()" v-model="lastName" required/>
+            <label for="lastName">
+              Last Name (using @input)
+              <span aria-hidden="true" class="required">*</span>
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              @input="$v.lastName.$touch()"
+              v-model="lastName"
+              required
+            />
             <p
               v-if="!$v.lastName.required && $v.lastName.$error"
               aria-live="assertive"
@@ -42,12 +59,15 @@
             <p
               v-if="!$v.lastName.alpha && $v.lastName.$error"
               aria-live="assertive"
-            >Last name must only contain alphabetic characters </p>
+            >Last name must only contain alphabetic characters</p>
           </div>
 
           <!-- Phone Number -->
           <div class="input" :class="{invalid: $v.phoneNumber.$error}">
-            <label for="phoneNumber">Phone Number (touch() in button) <span aria-hidden=true class="required">*</span></label>
+            <label for="phoneNumber">
+              Phone Number (touch() in button)
+              <span aria-hidden="true" class="required">*</span>
+            </label>
             <input
               type="tel"
               v-mask="'(###) ###-####'"
@@ -61,38 +81,74 @@
 
           <!-- Email -->
           <div class="input" :class="{invalid: $v.email.$error}">
-            <label for="email">Email <span aria-hidden=true class="required">*</span></label>
-            <input type="email" id="email" @blur="$v.email.$touch()" v-model="email" autocomplete="username email" required/>
+            <label for="email">
+              Email
+              <span aria-hidden="true" class="required">*</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              @blur="$v.email.$touch()"
+              v-model="email"
+              autocomplete="username email"
+              required
+            />
             <p v-if="!$v.email.email" aria-live="assertive">Must provide valid email</p>
           </div>
 
           <!-- Age -->
           <div class="input" :class="{invalid: $v.age.$error}">
-            <label for="age">Your Age <span aria-hidden=true class="required">*</span></label>
-            <input type="number" name="age" id="age" @blur="$v.age.$touch()" v-model.number="age" required/>
+            <label for="age">
+              Your Age
+              <span aria-hidden="true" class="required">*</span>
+            </label>
+            <input
+              type="number"
+              name="age"
+              id="age"
+              @blur="$v.age.$touch()"
+              v-model.number="age"
+              required
+            />
 
             <!-- Params will hold the params for the validator -->
             <p
-              v-if="!$v.age.maxValue && $v.age.$error" aria-live="assertive"
+              v-if="!$v.age.maxValue && $v.age.$error"
+              aria-live="assertive"
             >Cannot have age over {{$v.age.$params.maxValue.max}}</p>
             <p v-if="!$v.age.minValue && $v.age.$error">Must be over {{$v.age.$params.minValue.min}}</p>
             <p
-              v-if="!$v.age.between && $v.age.$error" aria-live="assertive"
+              v-if="!$v.age.between && $v.age.$error"
+              aria-live="assertive"
             >Age has to be between {{$v.age.$params.between.min}} and {{$v.age.$params.between.max}}</p>
           </div>
 
           <!-- Password -->
           <div class="input" :class="{invalid: $v.password.$error}">
-            <label for="password">Password <span aria-hidden=true class="required">*</span></label>
-            <input type="password" id="password" @blur="$v.password.$touch()" v-model="password" autocomplete="new-password" required/>
+            <label for="password">
+              Password
+              <span aria-hidden="true" class="required">*</span>
+            </label>
+            <input
+              type="password"
+              id="password"
+              @blur="$v.password.$touch()"
+              v-model="password"
+              autocomplete="new-password"
+              required
+            />
             <p
-              v-if="!$v.password.minLength && $v.password.$error" aria-live="assertive"
+              v-if="!$v.password.minLength && $v.password.$error"
+              aria-live="assertive"
             >Password must be at least {{$v.password.$params.minLength.min}} characters long</p>
           </div>
 
           <!-- Password Confirmation -->
           <div class="input" :class="{invalid: $v.confirmPassword.$error}">
-            <label for="confirm-password">Confirm Password <span aria-hidden=true class="required">*</span></label>
+            <label for="confirm-password">
+              Confirm Password
+              <span aria-hidden="true" class="required">*</span>
+            </label>
             <input
               type="password"
               id="confirm-password"
@@ -101,7 +157,10 @@
               required
               autocomplete="new-password"
             />
-            <p v-if="!$v.confirmPassword.sameAs && $v.confirmPassword.$error" aria-live="assertive">Passwords must match</p>
+            <p
+              v-if="!$v.confirmPassword.sameAs && $v.confirmPassword.$error"
+              aria-live="assertive"
+            >Passwords must match</p>
           </div>
 
           <!-- Secret Questions -->
@@ -120,7 +179,10 @@
               >x</button>
               <!-- Question -->
               <div :class="{invalid: $v.secretQuestionInputs.$each[index].value.$error}">
-                <label :for="secretQuestionInput.id">Secret Question #{{ index+1 }} <span aria-hidden=true class="required">*</span></label>
+                <label :for="secretQuestionInput.id">
+                  Secret Question #{{ index+1 }}
+                  <span aria-hidden="true" class="required">*</span>
+                </label>
                 <input
                   :class="{invalid: $v.secretQuestionInputs.$each[index].$error}"
                   type="text"
@@ -130,15 +192,17 @@
                   required
                 />
                 <p
-                  v-if="$v.secretQuestionInputs.$each[index].value.$error" aria-live="assertive"
+                  v-if="$v.secretQuestionInputs.$each[index].value.$error"
+                  aria-live="assertive"
                 >Input must be at least {{$v.secretQuestionInputs.$each[index].value.$params.minLength.min}} characters long</p>
               </div>
-              <br>
+              <br />
               <!-- Answers -->
               <div :class="{invalid: $v.secretQuestionInputs.$each[index].password.$error}">
-                <label
-                  :for="secretQuestionInput.id + 'password'"
-                >Secret Question #{{ index+1 }}'s Answer <span aria-hidden=true class="required">*</span></label>
+                <label :for="secretQuestionInput.id + 'password'">
+                  Secret Question #{{ index+1 }}'s Answer
+                  <span aria-hidden="true" class="required">*</span>
+                </label>
                 <input
                   :class="{invalid: $v.secretQuestionInputs.$each[index].$error}"
                   type="password"
@@ -148,23 +212,29 @@
                   required
                 />
                 <p
-                  v-if="$v.secretQuestionInputs.$each[index].password.$error" aria-live="assertive"
+                  v-if="$v.secretQuestionInputs.$each[index].password.$error"
+                  aria-live="assertive"
                 >Password must be at least {{$v.secretQuestionInputs.$each[index].password.$params.minLength.min}} characters long</p>
               </div>
             </div>
             <p
               v-if="!$v.secretQuestionInputs.minLength"
-              :class="{invalid: !$v.secretQuestionInputs.minLength}" aria-live="assertive"
+              :class="{invalid: !$v.secretQuestionInputs.minLength}"
+              aria-live="assertive"
             >You need at least {{$v.secretQuestionInputs.$params.minLength.min}} Secret Questions</p>
           </div>
           <!-- $iter present as direct child of $each objects -->
           <p
-            v-if="!$v.secretQuestionInputs.$each.$iter[0]" aria-live="assertive"
+            v-if="!$v.secretQuestionInputs.$each.$iter[0]"
+            aria-live="assertive"
           >You need at least {{$v.secretQuestionInputs.$params.minLength.min}} Secret Questions</p>
 
           <!-- Account Type -->
           <div class="input accountType">
-            <label for="accountType">Account Type <span aria-hidden=true class="required">*</span></label>
+            <label for="accountType">
+              Account Type
+              <span aria-hidden="true" class="required">*</span>
+            </label>
             <select id="accountType" v-model="accountType">
               <option value="personal">Personal</option>
               <option value="business">Business</option>
@@ -174,15 +244,24 @@
           <!-- Terms of Use -->
           <div class="input inline" :class="{invalid: $v.terms.$invalid}">
             <div>
-              <input type="checkbox" id="terms" @change="$v.terms.$touch()" v-model="terms"/>
-              <label for="terms">Accept Terms of Use <span v-if="$v.terms.$invalid" class="required" aria-label="required">*</span></label>
+              <input type="checkbox" id="terms" @change="$v.terms.$touch()" v-model="terms" />
+              <label for="terms">
+                Accept Terms of Use
+                <span v-if="$v.terms.$invalid" class="required" aria-label="required">*</span>
+              </label>
             </div>
 
             <!-- Initials -->
             <div class="input" :class="{invalid: $v.initials.$invalid}">
-              <label for="initials">Initials <span v-if="!$v.initials.required" class="required" aria-label="required">*</span></label>
-              <input type="text" id="initials" @blur="$v.initials.$touch()" v-model="initials"/>
-              <p v-if="!$v.initials.required && $v.initials.$error" aria-live="assertive">Initials are needed</p>
+              <label for="initials">
+                Initials
+                <span v-if="!$v.initials.required" class="required" aria-label="required">*</span>
+              </label>
+              <input type="text" id="initials" @blur="$v.initials.$touch()" v-model="initials" />
+              <p
+                v-if="!$v.initials.required && $v.initials.$error"
+                aria-live="assertive"
+              >Initials are needed</p>
             </div>
           </div>
 
@@ -228,26 +307,27 @@ export default {
     };
   },
   validations: {
-    firstName: { // has to have same name as the property you are binding to with v-model to automatially synchronize and know when value changes
+    firstName: {
+      // has to have same name as the property you are binding to with v-model to automatially synchronize and know when value changes
       required, // not empty
       minLength: minLength(2),
       alpha
     },
     lastName: {
-      required, 
+      required,
       minLength: minLength(2),
       alpha
     },
     phoneNumber: {
-      required, 
+      required,
       minLength: minLength(14)
     },
     email: {
-      required, 
-      email 
+      required,
+      email
     },
     age: {
-      required, 
+      required,
       numeric,
       minValue: minValue(18),
       maxValue: maxValue(120),
@@ -258,7 +338,7 @@ export default {
       minLength: minLength(6)
     },
     confirmPassword: {
-      required, 
+      required,
       sameAs: sameAs("password") //match this to your data property name
     },
     terms: {
